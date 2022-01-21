6ea6719760e25eba1fd534a34573cb4fa26ab833
newPacAttack <91351390+newPacAttack@users.noreply.github.com>
2022-01-21 10:55:38 -0700

    Add files via upload

---
+++ PRO-C42-template-main/Drops.js
@@ -0,0 +1,12 @@
+class Drops{
+    constructor(x,y){
+        
+            this.rain=Bodies.circle(x,y,5,options)
+    } 
+    
+    update(){
+        if(this.rain.position.y > height){
+            Matter.Body.setPosition(this.rain, {x:random(0,400), y:random(0,400)})
+        }
+    }
+}
